af6366589b6867afe7dfee230b1a458b289e1217
Ritee <pandyaritee31@gmail.com>
2024-07-19 00:34:46 +0530

    first commit

---
+++ division.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class division {
+	
+
+		public static void main(String[] args) {
+			int num1 = 5;
+			int num2 = 3;
+			
+			float res = (float) num1/num2;
+			
+			System.out.println(res);
+
+		}
+
+	}
+
+
---
+++ loops.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class loops {
+	
+
+		public static void main(String[] args) {
+			
+			for(int i = 0; i < 30 ;i++) {
+				if(i%2 != 0) {
+					System.out.println(i);
+				}
+			}
+		}
+
+	}
+
+
